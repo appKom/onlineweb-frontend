803325c21ce76670df37ba69e54ce3b35925b11e
oleast <oleast@stud.ntnu.no>
2019-08-23 18:34:09 +0200

    Refactor component for natification settings

--- src/profile/components/Settings/Notifications/index.tsx
+++ src/profile/components/Settings/Notifications/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ContextType } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 import { md } from 'common/components/Markdown';
 import { Pane } from 'common/components/Panes';
@@ -13,12 +13,11 @@ import {
   removeNotificationSubscription,
   verifyPushManager,
 } from 'common/utils/pushManager';
-
-import { UserContext } from 'authentication/providers/UserProvider';
-import { getAllChannels, getUserChannels, postUserChannels, subscribe, unsubscribe } from 'profile/api/notifications';
+import { useToast } from 'core/utils/toast/useToast';
+import { deleteSubscription, getSettings, getSubscriptions, postSubscription, updateSettings } from 'notifications/api';
+import { INotificationSetting, INotificationSubscription } from 'notifications/models/Notification';
 import { verifyServiceWorker } from 'serviceworker/browser';
 
-import { IChannel } from '../../../models/Notification';
 import BrowserSupport from './BrowserSupport';
 import style from './notifications.less';
 import Option from './Option';
@@ -28,8 +27,6 @@ const ABOUT_NOTIFICATIONS = md`
 
   Notifikasjoner kan brukes til å sende deg varsler for hendelser som skjer
   på nettsiden uten at du trenger å være på den direkte.
-
-  ## Denne funksjonaliteten har ikke blitt ferdigimplementert!
 `;
 
 const ABOUT_BROWSER_SUPPORT = md`
@@ -53,148 +50,122 @@ const ABOUT_NOTIFICATION_OPTIONS = md`
   Her kan du velge hvilke deler av nettsiden du ønsker å motta notifikasjoner.
 `;
 
-export interface IState {
-  channels: IChannel[];
-  selectedChannels: string[];
-  allowNotifications: boolean;
-  subscription?: PushSubscription;
-  message?: string;
-}
+export const Notifications: FC = () => {
+  const [allowNotifications, setAllowNotifications] = useState(resolveNotificationPermission());
+  const [settings, setSettings] = useState<INotificationSetting[]>([]);
+  const [subscriptions, setSubscriptions] = useState<INotificationSubscription[]>([]);
+  const [subscription, setSubscription] = useState<PushSubscription | undefined>(undefined);
+  const subscriptionEndpoint = subscription ? subscription.endpoint : '';
+  const currentSubscription = subscriptions.find((s) => s.endpoint === subscriptionEndpoint);
 
-class Notifications extends Component<{}, IState> {
-  public static contextType = UserContext;
-  public context!: ContextType<typeof UserContext>;
+  const [displayMessage] = useToast({ overwrite: true, type: 'info' });
 
-  public state: IState = {
-    allowNotifications: resolveNotificationPermission(),
-    channels: [],
-    selectedChannels: [],
+  const fetchSettings = async () => {
+    const data = await getSettings();
+    setSettings(data.results);
   };
 
-  public async componentDidMount() {
-    this.getCurrentSubscription();
-    this.getAllChannels();
-    this.getUserChannels();
-  }
-
-  public getAllChannels = async () => {
-    const { user } = this.context;
-    if (user) {
-      const channels = await getAllChannels(user);
-      this.setState({ channels });
-    }
+  const fetchSubscriptions = async () => {
+    const data = await getSubscriptions();
+    setSubscriptions(data.results);
   };
 
-  public getUserChannels = async () => {
-    const { user } = this.context;
-    if (user) {
-      const channels = await getUserChannels(user);
-      const selectedChannels = channels.map((channel) => channel.name);
-      this.setState({ selectedChannels });
-    }
-  };
-
-  public toggleChannel = (channelName: string) => {
-    const { selectedChannels } = this.state;
-    const channels = selectedChannels.includes(channelName)
-      ? selectedChannels.filter((i) => i !== channelName)
-      : [...selectedChannels, channelName];
-    this.setState({ selectedChannels: channels }, this.updateChannels);
-  };
-
-  public updateChannels = async () => {
-    const { user } = this.context;
-    const { selectedChannels } = this.state;
-    if (user) {
-      const channels = await postUserChannels(selectedChannels, user);
-      const selected = channels.map((channel) => channel.name);
-      this.setState({ selectedChannels: selected });
-    }
-  };
-
-  public toggleGlobalNotifications = async () => {
-    const permission = await getNotificationPermission();
-    this.setState({ allowNotifications: permission });
-  };
-
-  public getCurrentSubscription = async () => {
-    const subscription = await getNotificationSubscription();
-    if (subscription) {
-      this.setState({ subscription });
-    }
-  };
-
-  public subscribe = async () => {
-    const { subscription, message } = await registerPushManager();
-    this.setState({ message, subscription });
-
-    const { user } = this.context;
-    if (user && subscription) {
-      await subscribe(subscription, user);
-    }
-  };
-
-  public unsubscribe = async () => {
-    const { user } = this.context;
+  const fetchSubscriptionFromSW = async () => {
     const sub = await getNotificationSubscription();
-    if (user && sub) {
-      await unsubscribe(sub, user);
+    setSubscription(sub || undefined);
+  };
+
+  useEffect(() => {
+    fetchSubscriptions();
+    fetchSettings();
+    fetchSubscriptionFromSW();
+  }, []);
+
+  const toggleGlobalNotifications = async () => {
+    const permission = await getNotificationPermission();
+    setAllowNotifications(permission);
+  };
+
+  const subscribe = async () => {
+    const { subscription: newSubscription, message } = await registerPushManager();
+    setSubscription(newSubscription);
+    if (message) {
+      displayMessage(message);
+    }
+    if (newSubscription) {
+      await postSubscription(newSubscription);
+    }
+  };
+
+  const unsubscribe = async () => {
+    const sub = await getNotificationSubscription();
+    if (sub && currentSubscription) {
+      await deleteSubscription(currentSubscription.id);
       const removed = await removeNotificationSubscription();
       if (removed) {
-        this.setState({ subscription: undefined });
+        setSubscription(undefined);
       }
     }
   };
 
-  public render() {
-    const { channels, allowNotifications, subscription, message } = this.state;
-    const { selectedChannels } = this.state;
-    return (
-      <>
-        <Pane>{ABOUT_NOTIFICATIONS}</Pane>
-        <Pane>
-          {ABOUT_BROWSER_SUPPORT}
-          <div className={style.container}>
-            <BrowserSupport name="Notification" value={verifyNotification()} />
-            <BrowserSupport name="PushManager" value={verifyPushManager()} />
-            <BrowserSupport name="ServiceWorker" value={verifyServiceWorker()} />
-          </div>
-        </Pane>
-        <Pane>
-          {ABOUT_ENABLE_NOTIFICATIONS}
-          <div className={style.container}>
-            {message && md`**${message}**`}
+  const toggleSubscription = async () => {
+    if (currentSubscription) {
+      await unsubscribe();
+    } else {
+      await subscribe();
+    }
+    await fetchSubscriptions();
+  };
+
+  const patchSetting = async (setting: INotificationSetting) => {
+    await updateSettings(setting.id, { ...setting, push: !setting.push });
+    await fetchSettings();
+  };
+
+  return (
+    <>
+      <Pane>{ABOUT_NOTIFICATIONS}</Pane>
+      <Pane>
+        {ABOUT_BROWSER_SUPPORT}
+        <div className={style.container}>
+          <BrowserSupport name="Notification" value={verifyNotification()} />
+          <BrowserSupport name="PushManager" value={verifyPushManager()} />
+          <BrowserSupport name="ServiceWorker" value={verifyServiceWorker()} />
+        </div>
+      </Pane>
+      <Pane>
+        {ABOUT_ENABLE_NOTIFICATIONS}
+        <div className={style.container}>
+          <Option
+            description="Tillat notifikasjoner på dette nettstedet"
+            name="allowNotifications"
+            value={allowNotifications}
+            toggle={toggleGlobalNotifications}
+          />
+          <Option
+            description="Registrer denne enheten for å motta notifikasjoner"
+            name="subscription"
+            value={!!currentSubscription}
+            toggle={toggleSubscription}
+          />
+        </div>
+      </Pane>
+      <Pane>
+        {ABOUT_NOTIFICATION_OPTIONS}
+        <div className={style.container}>
+          {settings.map((setting) => (
             <Option
-              description="Tillat notifikasjoner på dette nettstedet"
-              name="allowNotifications"
-              value={allowNotifications}
-              toggle={this.toggleGlobalNotifications}
+              key={setting.message_type}
+              name={setting.verbose_type}
+              description={setting.verbose_type}
+              value={setting.push}
+              toggle={() => patchSetting(setting)}
             />
-            <Option
-              description="Registrer denne enheten for å motta notifikasjoner"
-              name="subscription"
-              value={!!subscription}
-              toggle={!!subscription ? this.unsubscribe : this.subscribe}
-            />
-          </div>
-        </Pane>
-        <Pane>
-          {ABOUT_NOTIFICATION_OPTIONS}
-          <div className={style.container}>
-            {channels.map((channel) => (
-              <Option
-                key={channel.name}
-                name={channel.name}
-                description={channel.description}
-                value={selectedChannels.includes(channel.name)}
-                toggle={() => this.toggleChannel(channel.name)}
-              />
-            ))}
-          </div>
-        </Pane>
-      </>
-    );
-  }
-}
+          ))}
+        </div>
+      </Pane>
+    </>
+  );
+};
 
 export default Notifications;
